a8444fb2ff50d8cdcf53ed6d13211360e7415fcd
= <=>
2026-06-07 04:44:57 -0300

    formatando a visualização de prévia

--- gerador_relatorios.py
+++ gerador_relatorios.py
@@ -92,7 +92,7 @@ def gerar_texto_rtf(df_final, resumo_livros, total_geral):
     # O formato de página do LibreOffice exige tanto o comando geral quanto o da seção (sxn)
     # 9cm = 5103 twips | 29.7cm = 16838 twips
     # Margens: 1cm = 567 | 0.43cm = 244
-    config_pagina = r"\landscape\paperh11906\paperw16838\margl567\margr244\margt567\margb238\sectd\pgwsxn16838\pghsxn11906\marglsxn567\margrsxn244\margtsxn567\margbsxn238"
+    config_pagina = r"\portrait\paperh5103\paperw16838\margl567\margr244\margt567\margb238\sectd\pgwsxn16838\pghsxn5103\marglsxn567\margrsxn244\margtsxn567\margbsxn238"
     
     template_rtf = f"""{{\\rtf1\\ansi\\deff0 
 {{\\fonttbl{{\\f0 Arial;}}}}
