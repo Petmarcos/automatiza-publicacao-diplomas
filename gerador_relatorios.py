442d29f86b65f6450be6ef85b40e91509903cae9
= <=>
2026-06-07 13:29:12 -0300

    formatando a visualização de prévia_V1

--- gerador_relatorios.py
+++ gerador_relatorios.py
@@ -1,62 +1,21 @@
-import pandas as pd
-from datetime import datetime
-from collections import namedtuple
+def gerar_texto_rtf(df_final, resumo_livros, total_geral):
+    # ... (lógica de data/livros igual) ...
 
-# 1. Funções de suporte inalteradas
-def calcular_resumo_livros(df_final):
-    df_final['Registro da homologação'] = pd.to_numeric(df_final['Registro da homologação'], errors='coerce')
-    resumo = df_final.groupby('Livro').agg(
-        Total_Registros=('Registro da homologação', 'count'),
-        Primeiro_Registro=('Registro da homologação', 'min'),
-        Ultimo_Registro=('Registro da homologação', 'max')
-    ).reset_index()
-    LinhaResumo = namedtuple('LinhaResumo', ['Livro', 'Total_Registros', 'Primeiro_Registro', 'Ultimo_Registro'])
-    return [LinhaResumo(**row) for row in resumo.to_dict(orient='records')]
-
-def descobrir_mes_referencia(df_final):
-    try:
-        datas = pd.to_datetime(df_final['Homologacao'], errors='coerce')
-        datas_validas = datas.dropna()
-        if not datas_validas.empty:
-            data_ref = datas_validas.iloc[0]
-            return data_ref.strftime("%B").lower(), data_ref.strftime("%Y")
-    except: pass
-    hoje = datetime.now()
-    return hoje.strftime("%B").lower(), str(hoje.year)
-
-# 2. A FUNÇÃO "À PROVA DE ERROS"
-# O uso de *args e **kwargs faz com que ela aceite QUALQUER número de argumentos
-def gerar_texto_rtf(*args, **kwargs):
-    # Recupera os dados dos argumentos, não importa como foram passados
-    df_final = args[0]
-    resumo_livros = args[1]
-    total_geral = args[2]
+    # 1. Definimos a Calibri como \f0 na tabela de fontes
+    # 2. \fs18 define o tamanho 9 (18 meios-pontos)
+    config_pagina = r"\sectd\paperh11906\paperw16838\margl567\margr244\margt567\margb238\pgwsxn16838\pghsxn11906\marglsxn567\margrsxn244\margtsxn567\margbsxn238\fs18"
     
-    # Tenta pegar os novos argumentos se existirem, senão calcula na hora
-    mes_extenso, ano = descobrir_mes_referencia(df_final)
-    mes_referencia = kwargs.get('mes_referencia', f"{mes_extenso} de {ano}")
-    data_assinatura = kwargs.get('data_assinatura', datetime.now().strftime("%d de %B de %Y"))
-    
-    trechos = [f"livro {r.Livro} com {r.Total_Registros} registros numerados no intervalo de {r.Primeiro_Registro} a {r.Ultimo_Registro}" for r in resumo_livros]
-    texto_livros_corrido = "; ".join(trechos)
-
-    config_pagina = r"\landscape\paperh5103\paperw16838\margl567\margr244\margt567\margb238"
-    
-    return f"""{{\\rtf1\\ansi\\deff0 
+    template_rtf = f"""{{\\rtf1\\ansi\\deff0 
 {{\\fonttbl{{\\f0 Calibri;}}}}
 {config_pagina}
-\\pard\\qc\\b ##ATO AVISO DE REGISTRO DE DIPLOMAS\\b0\\par
-\\pard\\qc\\fs18 \\par
-\\pard\\qj\\fi567\\li0\\b ##TEX\\b0  \\tab O Instituto Capivara Learning, CNPJ no 10.738.898/0001-75, em atendimento ao disposto no art. 21 da Portaria MEC n° 1.095 de 25 de outubro de 2018 informa que, no mes de {mes_referencia} do corrente ano, registrou {total_geral} diplomas assim distribuidos: {texto_livros_corrido}.\\par
-\\pard\\qj\\fi567\\li0 \\tab A relacao dos diplomas registrados podera ser consultada em ate trinta dias, no endereco eletronico https://www.icl.edu.br/pre/controle-academico/erd.\\par
-\\pard\\par
-\\pard\\qc\\b ##DAT Joao Pessoa, {data_assinatura}\\b0\\par
-\\pard\\par
-\\pard\\qc\\b ##ASS Capivara Svenson\\b0\\par
-\\pard\\qc\\b ##CAR Reitora\\b0\\par
+{{\\b ##ATO\\b0  AVISO DE REGISTRO DE DIPLOMAS}}\\par
+{{\\b ##TEX\\b0  O Instituto Capivara Learning, CNPJ no 10.738.898/0001-75, em atendimento ao disposto no art. 21 da Portaria MEC n° 1.095 de 25 de outubro de 2018 informa que, no mes de {mes_referencia} do corrente ano, registrou {total_geral} diplomas assim distribuidos: {texto_livros_corrido}.}}\\par
+A relacao dos diplomas registrados podera ser consultada em ate trinta dias, no endereco eletronico https://www.icl.edu.br/pre/controle-academico/erd.\\par
+\\par
+{{\\b ##DAT\\b0  Joao Pessoa, {data_assinatura}}}\\par
+\\par
+{{\\b ##ASS\\b0  Capivara Svenson}}\\par
+{{\\b ##CAR\\b0  Reitora}}\\par
 }}"""
-
-# 3. CHAMADA (Chame da forma mais simples possível)
-resumo = calcular_resumo_livros(df_final)
-total = df_final.shape[0]
-conteudo_rtf = gerar_texto_rtf(df_final, resumo, total)
+    
+    return template_rtf
