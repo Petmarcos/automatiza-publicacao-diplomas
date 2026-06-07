dbb25351c333a5bebf3cbb202843600fdfe7635f
= <=>
2026-06-07 11:52:31 -0300

    formatando a visualização de prévia

--- gerador_relatorios.py
+++ gerador_relatorios.py
@@ -57,25 +57,22 @@ def descobrir_mes_referencia(df_final):
     return data_ficticia.strftime("%B").lower(), str(ano_anterior)
 
 
-def gerar_texto_rtf(df_final, resumo_livros, total_geral):
+def gerar_texto_rtf(df_final, resumo_livros, total_geral, mes_referencia, data_assinatura):
     # ... (lógica de data/meses/livros permanece a mesma) ...
 
     # Margens e dimensões
     config_pagina = r"\landscape\paperh5103\paperw16838\margl567\margr244\margt567\margb238"
     
     template_rtf = f"""{{\\rtf1\\ansi\\deff0 
-{{\\fonttbl{{\\f0 Calibri;}}}}
-{config_pagina}
-\\pard\\qc\\b ##ATO AVISO DE REGISTRO DE DIPLOMAS\\b0\\par
-\\pard\\qc\\fs18 \\par
-\\pard\\qj\\fi567\\li0\\b ##TEX\\b0  \\tab O Instituto Capivara Learning, CNPJ no 10.738.898/0001-75, em atendimento ao disposto no art. 21 da Portaria MEC n° 1.095 de 25 de outubro de 2018 informa que, no mes de {mes_referencia} do corrente ano, registrou {total_geral} diplomas assim distribuidos: {texto_livros_corrido}.\\par
-\\pard\\qj\\fi567\\li0 \\tab A relacao dos diplomas registrados podera ser consultada em ate trinta dias, no endereco eletronico https://www.icl.edu.br/pre/controle-academico/erd.\\par
-\\pard\\par
-\\pard\\qc\\b ##DAT Joao Pessoa, {data_assinatura}\\b0\\par
-\\pard\\par
-\\pard\\qc\\b ##ASS Capivara Svenson\\b0\\par
-\\pard\\qc\\b ##CAR Reitora\\b0\\par
-}}"""
+    {{\\fonttbl{{\\f0 Calibri;}}}}
+    {config_pagina}
+    \\pard\\qc\\b ##ATO AVISO DE REGISTRO DE DIPLOMAS\\b0\\par
+    \\par
+    \\pard\\qj\\fi567\\li0\\b ##TEX\\b0  \\tab O Instituto Capivara Learning, CNPJ no 10.738.898/0001-75, em atendimento ao disposto no art. 21 da Portaria MEC n° 1.095 de 25 de outubro de 2018 informa que, no mes de {mes_referencia} do corrente ano, registrou {total_geral} diplomas assim distribuidos: {texto_livros_corrido}.\\par
+    ...
+    \\pard\\qc\\b ##DAT Joao Pessoa, {data_assinatura}\\b0\\par
+    ...
+    }}"""
     
     return template_rtf
     
